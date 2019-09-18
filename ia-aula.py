04032990fca56bc636c26e7586192b8591dde680
Joas Feitosa <joasfeitosadelimasilva@gmail.com>
2019-09-17 23:22:08 -0300

    Exercicio prático de Inteligencia Artificial

---
+++ ia-aula.py
@@ -0,0 +1,20 @@
+# IA
+
+import matplotlib.pyplot as plt
+import numpy as np
+%matplotlib inline
+
+#diâmetro (cm)
+diametros = [[7], [10], [15], [30], [45]]
+
+#preços 
+precos = [[8], [11], [16], [38.5], [52]]
+
+plt.figure()
+plt.xlabel('Diâmetros(ccm)')
+plt.ylabel('Preços(R$)')
+plt.title('Diâmetro x Preço')
+plt.plot(diametros, precos, 'k.')
+plt.axis([0, 60, 0, 60])
+plt.grid(True)
+plt.show()
